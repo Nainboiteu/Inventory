3061973510ad7e3dcd515fffb8dea14853480655
Nainboiteu <jeanbaptiste.gobin@gmail.com>
2023-08-29 14:22:11 +0200

    v1.2

--- assets/JSON/jsonParser.js
+++ assets/JSON/jsonParser.js
@@ -1,12 +1,22 @@
 fetch('db/data.json')
-  .then(response => response.json())
-  .then(parsedData => {
-    // Log the parsed JSON data to the console
-    console.log(parsedData);
-    
-    // You can also use JSON.stringify for formatted output
-    console.log(JSON.stringify(parsedData, null, 2));
-  })
-  .catch(error => {
-    console.error('Error fetching or parsing JSON:', error);
-  });
+    .then(response => response.json())
+    .then(parsedData => {
+        // Log the parsed JSON data to the console
+        console.log(parsedData);
+
+        // You can also use JSON.stringify for formatted output
+        console.log(JSON.stringify(parsedData, null, 2));
+    })
+    .catch(error => {
+        console.error('Error fetching or parsing JSON:', error);
+    });
+
+function generateArticles(parsedData) {
+    const container = document.getElementById('container');
+    jsonData.forEach(item => {
+        const div = document.createElement('div');
+        div.className = 'item'; // You can apply CSS classes here
+        div.innerHTML = `<h2>${item.name}</h2><p>${item.description}</p>`;
+        container.appendChild(div);
+    });
+}
